0fded53db7d925eda58f50f2db13663ae42f091e
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-12-07 01:17:04 -0500

    added gitignore

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 configRoutes(app);
 
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("We've now got a server!");
   console.log("Your routes will be running on http://localhost:3000");
 });
--- config/MongoConnection.js
+++ config/MongoConnection.js
@@ -1,4 +1,3 @@
-
 const MongoClient = require("mongodb").MongoClient;
 const settings = {
   mongoConfig: {
@@ -13,7 +12,10 @@ let _db = undefined;
 
 module.exports = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl,{ useNewUrlParser: true });
+    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     _db = await _connection.db(mongoConfig.database);
   }
   return _db;
